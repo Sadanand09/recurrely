12ea430952d3d334a96c61a7ad5a8c8d71d1234b
Sadanand09 <sadanandgurude9@gmail.com>
2026-04-07 15:25:01 +0530

    Project & Nativewind setup

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import "@/global.css";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className=" bg-background">
+      <Text className="text-success">Welcome to Nativewind!</Text>
+    </View>
+  );
+}
